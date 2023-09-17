ad912164bc7a2142efe82787eed510fe1a1537bb
Chfia <marhvitka@wp.pl>
2023-09-17 12:46:32 +0200

    New Arena Helper

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,67 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h1>Vue.js Log Filter</h1>
+
+    <!-- Pole tekstowe do wklejania logów -->
+    <textarea v-model="logsText" placeholder="Wklej logi tutaj"></textarea>
+
+    <!-- Przycisk do uruchomienia filtrowania -->
+    <button @click="filterLogs">Filtruj</button>
+
+    <!-- Wyświetlanie przefiltrowanych logów -->
+    <div>
+      <h2>Przefiltrowane Logi:</h2>
+      <ul>
+        <li v-for="(log, index) in filteredLogs" :key="index" v-html="log"></li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      logsText: '', // Dane wklejone przez użytkownika
+      filteredLogs: [] // Przechowuje przefiltrowane logi
+    };
+  },
+  methods: {
+    filterLogs() {
+      // Rozdziel logi na linie
+      const lines = this.logsText.split('\n');
+      const filteredLogs = [];
+
+      let currentLog = ''; // Przechowuje bieżący log
+
+      // Lista zwierząt do odfiltrowania
+      const zwierzetaDoFiltrowania = ['Wilk', 'Jeleń', 'Lis', 'Przepiórka', 'Dzik', 'PRZEGRAŁEŚ', 'Słoń', 'Szejk Am-Al', 'przestępca', 'zwierzę' ];
+
+      // Przeglądaj każdą linię
+      for (const line of lines) {
+        if (line.match(/^\d+\.\s\d+/)) {
+          // Jeśli linia rozpoczyna nowy log, dodaj poprzedni log do wyników
+          if (currentLog.trim() !== '' && !zwierzetaDoFiltrowania.some(zwierze => currentLog.includes(zwierze))) {
+            filteredLogs.push(currentLog.trim());
+          }
+          currentLog = line;
+        } else {
+          // W przeciwnym razie dodaj linię do bieżącego logu
+          currentLog += '<br>' + line;
+        }
+      }
+
+      // Dodaj ostatni log, jeśli nie zawiera zwierzęcia
+      if (currentLog.trim() !== '' && !zwierzetaDoFiltrowania.some(zwierze => currentLog.includes(zwierze))) {
+        filteredLogs.push(currentLog.trim());
+      }
+
+      this.filteredLogs = filteredLogs;
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+/* Możesz dodać stylizację CSS tutaj */
 </style>
